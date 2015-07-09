07cee32f2971fdd86e2ebebdf267c37dff4abfbc
jsoncode <jsoncode@users.noreply.github.com>
2015-07-09 15:12:15 +0800

    使用canvas画一个任意角度的扇形

---
+++ canvasSector.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @authors jsoncode (http://jsoncode.com)
+ * @date    2015-07-09 15:11:40
+ * @version $Id$
+ */
+
+CanvasRenderingContext2D.prototype.sector=function(x,y,r,sAngle,eAngle,color){
+	this.moveTo(x,y);
+	this.arc(x,y,r,sAngle,eAngle);
+	this.lineTo(x,y);
+	this.fillStyle=color;
+	this.fill();
+	this.save();
+}
+
+// 获取标签
+var bgcanvas = document.querySelector("#bgcanvas");
+var ctx = bgcanvas.getContext("2d");
+ctx.sector(250,250,200,0,2*Math.PI,"#fff");
